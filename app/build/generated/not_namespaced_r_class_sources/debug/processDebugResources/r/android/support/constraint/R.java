67df74565c3b25fbccc7e335e7a4462760784a24
psalmfill <samfield4sure@gmail.com>
2019-01-21 04:39:44 +0100

    added login to app

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f080024;
-        public static final int end = 0x7f080042;
-        public static final int gone = 0x7f080050;
-        public static final int invisible = 0x7f08005a;
-        public static final int left = 0x7f08005f;
-        public static final int packed = 0x7f080073;
-        public static final int parent = 0x7f080075;
-        public static final int percent = 0x7f080078;
-        public static final int right = 0x7f080086;
-        public static final int spread = 0x7f0800a9;
-        public static final int spread_inside = 0x7f0800aa;
-        public static final int start = 0x7f0800af;
-        public static final int top = 0x7f0800c8;
-        public static final int wrap = 0x7f0800d7;
+        public static final int end = 0x7f080043;
+        public static final int gone = 0x7f080051;
+        public static final int invisible = 0x7f08005c;
+        public static final int left = 0x7f080061;
+        public static final int packed = 0x7f080076;
+        public static final int parent = 0x7f080078;
+        public static final int percent = 0x7f08007c;
+        public static final int right = 0x7f08008a;
+        public static final int spread = 0x7f0800ad;
+        public static final int spread_inside = 0x7f0800ae;
+        public static final int start = 0x7f0800b3;
+        public static final int top = 0x7f0800cf;
+        public static final int wrap = 0x7f0800de;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/com/samfieldhawb/recipes/LoginActivity.java
+++ app/src/main/java/com/samfieldhawb/recipes/LoginActivity.java
@@ -1,13 +1,47 @@
 package com.samfieldhawb.recipes;
 
+import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-
+    TextInputEditText mEmail;
+    TextInputEditText mPassword;
+    Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+        mButton = findViewById(R.id.login);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                if(email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    mEmail.setError("Enter test@samfield.com as email address");
+                }
+                if(password.isEmpty() || password.length() < 5){
+                    mPassword.setError("Enter 1234 as  password");
+
+                }
+
+                if(email.equals("test@samfield.com") && password.equals("1234")){
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(LoginActivity.this,"Invalid Login Details",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
